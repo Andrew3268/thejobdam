7245ae8ab87091121085c9bc6eef7f5d45ed104a
이이정훈 <iijeonghun@192-168-1-12.tpgi.com.au>
2018-03-28 15:52:10 +1000

    First Comment

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,71 @@
+class PostsController < ApplicationController
+  
+  before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    if params[:category].blank?
+      @posts = Post.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    end
+  end
+
+  def show
+    @comments = Comment.where(post_id: @post)
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  def upvote
+    @post.upvote_by current_user
+    redirect_to post_path
+  end
+
+  def downvote
+    @post.downvote_by current_user
+    redirect_to post_path
+  end
+
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :link, :description, :image, :category_id)
+  end
+
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  acts_as_votable
+  belongs_to :user
+  belongs_to :category
+  has_many :comments
+
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates_presence_of :category_id, presence: true
+end
